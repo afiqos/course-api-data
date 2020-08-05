4741df93db0f604e88c117ab0a5188c8fe80870d
afiqos <mdafiqosman@gmail.com>
2020-08-05 15:00:17 +0800

    Adding Course APIs

---
+++ src/main/java/app/course/CourseController.java
@@ -0,0 +1,41 @@
+package app.course;
+
+import app.topic.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;  // injects instance of @Service created of TopicService.java into this
+
+    @RequestMapping("/topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id) {
+        return courseService.getAllCourses(id);
+    }
+
+    @RequestMapping("/topics/{topicId}/courses/{id}")   // we only need id for course as its the pkey
+    public Course getCourse(@PathVariable  String id) {
+        return courseService.getCourse(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT, value="/topics/{topicId}/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String id) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.updateCourse(course);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE, value="/topics/{topicId}/courses/{id}")
+    public void deleteCourse(@PathVariable String id) {
+        courseService.deleteCourse(id);
+    }
+}
---
+++ src/main/java/app/course/CourseRepository.java
@@ -0,0 +1,8 @@
+package app.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+
+}
